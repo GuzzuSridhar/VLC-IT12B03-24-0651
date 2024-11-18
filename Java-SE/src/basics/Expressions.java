d2f4a34a2a1d70895824cd79ab500c391e3a4ed6
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-18 13:03:14 +0530

    add expressions

---
+++ Java-SE/src/basics/Expressions.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class Expressions {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        // using the variables in an expression
+        int result = x + y; // assigning the return of the expression to a variable
+        System.out.println(result);
+        System.out.println(x + y); // printing the output of expression to the console
+
+    }
+}
