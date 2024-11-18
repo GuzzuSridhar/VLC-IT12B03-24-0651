0c8f23b75244ff72a17c38f2b52f55c9ff0368e5
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-18 15:26:35 +0530

    add boxing

---
+++ Java-SE/src/basics/BoxingAndUnboxing.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class BoxingAndUnboxing {
+    public static void main(String[] args) {
+        // boxing (implicit) convert primitive to wrapper class
+        int num = 10;
+        Integer test = num;
+
+        // unboxing (implicit) converting the wrapper class to primitive
+        Integer x = 10;
+        int y = x;
+
+    }
+}
---
+++ Java-SE/src/basics/WrapperClasses.java
@@ -0,0 +1,47 @@
+package basics;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+        // a wrapper class for all primitive types is available
+        // a wrapper class is a object notation of a primitive type
+
+        /*
+         * byte -- Byte
+         * short -- Short
+         * int -- Integer
+         * long -- Long
+         * float -- Float
+         * double -- Double
+         * char -- Character
+         * boolean -- Boolean
+         */
+
+        System.out.println("----------Byte Type -----------------");
+        System.out.println("The size of Byte is: " + Byte.SIZE);
+        System.out.println("The min value of Byte is: " + Byte.MIN_VALUE);
+        System.out.println("The max value of Byte is: " + Byte.MAX_VALUE);
+        System.out.println("----------Short Type -----------------");
+        System.out.println("The size of Short is: " + Short.SIZE);
+        System.out.println("The min value of Short is: " + Short.MIN_VALUE);
+        System.out.println("The max value of Short is: " + Short.MAX_VALUE);
+        System.out.println("----------Integer Type -----------------");
+        System.out.println("The size of Integer is: " + Integer.SIZE);
+        System.out.println("The min value of Integer is: " + Integer.MIN_VALUE);
+        System.out.println("The max value of Integer is: " + Integer.MAX_VALUE);
+        System.out.println("----------Long Type -----------------");
+        System.out.println("The size of Long is: " + Long.SIZE);
+        System.out.println("The min value of Long is: " + Long.MIN_VALUE);
+        System.out.println("The max value of Long is: " + Long.MAX_VALUE);
+        System.out.println("----------Float Type -----------------");
+        System.out.println("The size of Float is: " + Float.SIZE);
+        System.out.println("The min value of Float is: " + Float.MIN_VALUE);
+        System.out.println("The max value of Float is: " + Float.MAX_VALUE);
+        System.out.println("----------Double Type -----------------");
+        System.out.println("The size of Double is: " + Double.SIZE);
+        System.out.println("The min value of Double is: " + Double.MIN_VALUE);
+        System.out.println("The max value of Double is: " + Double.MAX_VALUE);
+        System.out.println("----------Character Type -----------------");
+        System.out.println("The size of Character is: " + Character.SIZE);
+
+    }
+}
