83a7646f23b6adbac4a0a6e57284b8128bf5e4b1
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-18 10:27:37 +0530

    add variables

---
+++ Java-SE/src/basics/Variables.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Declareing Variables
+        // Syntax
+        // <type> <name> = <default value>;
+
+        // example of a number type variable named "age"
+        int age = 10;
+        // printing the value of the age variable to console
+        System.out.println(age);
+
+        // variable to store text info
+        String name = "AnyOne";
+        System.out.println(name);
+
+    }
+}
