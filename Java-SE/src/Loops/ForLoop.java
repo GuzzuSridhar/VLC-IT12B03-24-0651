ead54f82f34eab5385fa52b3b9351c21b5055d84
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-19 13:49:12 +0530

    add for loop

---
+++ Java-SE/src/Loops/ForLoop.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+
+        // print numbers in reverse order
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
