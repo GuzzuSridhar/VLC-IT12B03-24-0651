5c7120468bd219435462b26cefd009e8f29637a0
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-19 06:59:39 +0530

    add special numbers

---
+++ Java-SE/src/basics/SpecialNumbers.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class SpecialNumbers {
+    public static void main(String[] args) {
+        // binary numbers (base 2)
+        int binaryNumber = 0b11;
+        System.out.println("the integer equivivalent of '0b11' is : " + binaryNumber);
+
+        // octal numbers (base 8)
+        int octalNumber = 011;
+        System.out.println("the integer equivivalent of '011' is : " + octalNumber);
+
+        // hexadecimal numbers (base 16)
+        int hex = 0xAA;
+        System.out.println("the integer equivivalent of '0xAA' is : " + hex);
+
+    }
+}
