d9f08d53b6bc348333bd2bd9134f24ebd8f2606d
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-20 07:18:13 +0530

    add arrays

---
+++ Java-SE/src/arrays/CreateArrays.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class CreateArrays {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        // ways to declare arrays
+
+        // declare an array instantiate of size 5
+        int[] numbers = { 100, 101, 102, 103, 104 };
+
+        // declare an array and instantiation seperately
+        int[] salaries; // declare an array
+        salaries = new int[] { 9899, 4566, 8888 }; // instantiate the values
+
+        // declaring an array with a specific size and instantiate programatically.
+        String[] names = new String[5]; // blank array of 5 elements
+        // or
+        int[] tests;
+        tests = new int[5];
+
+    }
+}
