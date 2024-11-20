7edfd22bb939769782cbbe0ce76e867cab8c30d4
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-20 12:39:06 +0530

    add classes

---
+++ Java-SE/src/classesandobjects/Book.java
@@ -0,0 +1,12 @@
+package classesandobjects;
+
+public class Book { // creating the blueprint
+    // feilds or also called as properties or members or instance variables
+    String title;
+    String author;
+    int yearOfPublication;
+    String genre;
+    int numberOfPages;
+}
+
+// not mandatory to have main method
---
+++ Java-SE/src/classesandobjects/Library.java
@@ -0,0 +1,47 @@
+package classesandobjects;
+
+public class Library {
+    public static void main(String[] args) {
+        // creating the instances or objects of Book type
+        Book b001 = new Book();
+        Book b002 = new Book();
+
+        // populate the information of book for each instance
+
+        // populating b001
+        b001.title = "Let us C";
+        b001.author = "Yeshwant Kanetkar";
+        b001.yearOfPublication = 1989;
+        b001.genre = "programming";
+        b001.numberOfPages = 598;
+
+        // populating b002
+        b002.title = "Let us CPP";
+        b002.author = "Yeshwant Kanetkar";
+        b002.yearOfPublication = 1989;
+        b002.genre = "programming";
+        b002.numberOfPages = 659;
+
+        // access the information of b001
+        System.out.println(b001.title
+                + " "
+                + b001.author
+                + " "
+                + b001.yearOfPublication
+                + " "
+                + b001.numberOfPages
+                + " "
+                + b001.genre);
+        // access the information of b002
+        System.out.println(b002.title
+                + " "
+                + b002.author
+                + " "
+                + b002.yearOfPublication
+                + " "
+                + b002.numberOfPages
+                + " "
+                + b002.genre);
+
+    }
+}
