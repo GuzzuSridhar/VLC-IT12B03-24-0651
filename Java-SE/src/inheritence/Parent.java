1c9ee05d09d68c75876c4eacefa44eef57a80445
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-21 09:18:47 +0530

    add inheritence

---
+++ Java-SE/src/inheritence/Parent.java
@@ -0,0 +1,24 @@
+package inheritence;
+
+public class Parent {
+    String parentVar;
+
+    // create the default constructor
+    public Parent() {
+        System.out.println("Parent Constructor called");
+    }
+
+    // method declaration
+    void parentMethod() {
+        System.out.println("Parent Method called");
+    }
+
+    void favFood() {
+        System.out.println("fish");
+    }
+
+    void occupation() {
+        System.out.println("Employee");
+    }
+
+}
