23d2321b14b32dae45436f02cd2f7b07d834e78f
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-21 07:39:47 +0530

    add access modifiers

--- Java-SE/src/accessmodifiers/packageone/ClassOne.java
+++ Java-SE/src/accessmodifiers/packageone/ClassOne.java
@@ -1,5 +1,22 @@
 package accessmodifiers.packageone;
 
-public class ClassOne {
+import accessmodifiers.packagetwo.ClassTwo;
 
+// check the access modifiers outside the package
+public class ClassOne {
+    public static void main(String[] args) {
+        ClassTwo classTwo = new ClassTwo();
+        // access all of the variables
+        System.out.println(classTwo.publicVar);
+        // System.out.println(classTwo.privateVar); // not allowed
+        // System.out.println(classTwo.defaultVar); // not allowed
+        // System.out.println(classTwo.protectedVar); // not allowed
+
+        // access all of the methods
+        classTwo.publicMethod();
+        // classTwo.privateMethod(); // not allowed
+        // classTwo.defaultMethod(); // not allowed
+        // classTwo.protectedMethod(); // not allowed
+
+    }
 }
---
+++ Java-SE/src/accessmodifiers/packagetwo/SubClass.java
@@ -0,0 +1,19 @@
+package accessmodifiers.packagetwo;
+
+public class SubClass extends ClassTwo {
+    public static void main(String[] args) {
+        ClassTwo classTwo = new ClassTwo();
+        // access all of the variables
+        System.out.println(classTwo.publicVar);
+        // System.out.println(classTwo.privateVar);
+        System.out.println(classTwo.defaultVar);
+        System.out.println(classTwo.protectedVar);
+
+        // access all of the methods
+        classTwo.publicMethod();
+        // classTwo.privateMethod();
+        classTwo.defaultMethod();
+        classTwo.protectedMethod();
+
+    }
+}
