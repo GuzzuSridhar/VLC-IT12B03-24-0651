04511a32a8d9fe46085eaaeada7994f3494367cb
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-21 07:46:13 +0530

    add am

--- Java-SE/src/accessmodifiers/packageone/SubClass.java
+++ Java-SE/src/accessmodifiers/packageone/SubClass.java
@@ -2,20 +2,24 @@ package accessmodifiers.packageone;
 
 import accessmodifiers.packagetwo.ClassTwo;
 
-public class SubClass {
+// access modifiers out side the package in a subclass
+public class SubClass extends ClassTwo {
     public static void main(String[] args) {
         ClassTwo classTwo = new ClassTwo();
+        SubClass subClass = new SubClass();
         // access all of the variables
         System.out.println(classTwo.publicVar);
         // System.out.println(classTwo.privateVar);
         // System.out.println(classTwo.defaultVar);
         // System.out.println(classTwo.protectedVar);
+        System.out.println(subClass.protectedVar);
 
         // access all of the methods
         classTwo.publicMethod();
         // classTwo.privateMethod();
         // classTwo.defaultMethod();
         // classTwo.protectedMethod();
+        subClass.protectedMethod();
 
     }
 }
