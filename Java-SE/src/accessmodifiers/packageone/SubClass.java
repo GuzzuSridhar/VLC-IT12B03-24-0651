917a90d8d8896867c7df79976fa90415a8cd6c9c
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-21 07:55:11 +0530

    add subclass changes

--- Java-SE/src/accessmodifiers/packageone/SubClass.java
+++ Java-SE/src/accessmodifiers/packageone/SubClass.java
@@ -7,18 +7,31 @@ public class SubClass extends ClassTwo {
     public static void main(String[] args) {
         ClassTwo classTwo = new ClassTwo();
         SubClass subClass = new SubClass();
+
         // access all of the variables
         System.out.println(classTwo.publicVar);
         // System.out.println(classTwo.privateVar);
         // System.out.println(classTwo.defaultVar);
         // System.out.println(classTwo.protectedVar);
-        System.out.println(subClass.protectedVar);
 
         // access all of the methods
         classTwo.publicMethod();
         // classTwo.privateMethod();
         // classTwo.defaultMethod();
         // classTwo.protectedMethod();
+
+        // using the SubClass Instance
+
+        // access all of the variables
+        System.out.println(subClass.publicVar);
+        // System.out.println(classTwo.privateVar);
+        // System.out.println(subClass.defaultVar);
+        System.out.println(subClass.protectedVar);
+
+        // access all of the methods
+        subClass.publicMethod();
+        // classTwo.privateMethod();
+        // subClass.defaultMethod();
         subClass.protectedMethod();
 
     }
