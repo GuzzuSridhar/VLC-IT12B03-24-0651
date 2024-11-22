b0086d3108341984c65f4da6739a8b5175144022
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-22 07:25:17 +0530

    add date app

---
+++ Java-SE/src/dates/EmpList.java
@@ -0,0 +1,27 @@
+package dates;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EmpList {
+    public static void main(String[] args) {
+        int id = 0;
+        String name = "", dt = "";
+        ArrayList<Employee> employees = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the ID: ");
+        id = scan.nextInt();
+        scan.nextLine();
+        System.out.print("Enter the Name: ");
+        name = scan.nextLine();
+        System.out.print("Enter the Date (yyyy-MM-dd): ");
+        dt = scan.nextLine();
+        employees.add(new Employee(id, name, LocalDate.parse(dt)));
+
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+
+    }
+}
