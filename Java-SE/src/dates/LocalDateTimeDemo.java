26d4dac4c5ed1f6a46f023b6ffefd334145cbb09
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-22 07:15:39 +0530

    add local DateTime

---
+++ Java-SE/src/dates/LocalDateTimeDemo.java
@@ -0,0 +1,14 @@
+package dates;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeDemo {
+    public static void main(String[] args) {
+        // current date and time
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+    }
+
+    // all methods to access the parameters of date and time objects
+    // will be the same as in LocalDate and LocalTime classes
+}
